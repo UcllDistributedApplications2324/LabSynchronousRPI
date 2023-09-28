14cdf95255a14d535a7d6dcb75e6737bc8f0db91
Bram Van Impe <bram.vanimpe@ucll.be>
2023-09-28 16:01:41 +0200

    Lab setup

--- appointment-service/src/main/java/be/ucll/da/appointmentservice/domain/AppointmentException.java
+++
@@ -1,8 +0,0 @@
-package be.ucll.da.appointmentservice.domain;
-
-public class AppointmentException extends RuntimeException {
-
-    public AppointmentException(String message) {
-        super(message);
-    }
-}
